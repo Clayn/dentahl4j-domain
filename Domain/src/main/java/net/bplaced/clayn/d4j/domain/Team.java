e154922851306ca6f6687f99dec8b6e5d4a814e5
Clayn <clayn_osmato@gmx.de>
2019-03-31 17:50:34 +0200

    dev
    Improved the team class with some properties

--- Domain/src/main/java/net/bplaced/clayn/d4j/domain/Team.java
+++ Domain/src/main/java/net/bplaced/clayn/d4j/domain/Team.java
@@ -11,11 +11,50 @@ import java.util.Map;
 public class Team
 {
 
+    private String name;
+    private String description;
+    private int id;
     private final Map<Integer, Ninja> positions = new HashMap<>();
 
     public Map<Integer, Ninja> getPositions()
     {
         return positions;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Team{" + "name=" + name + ", description=" + description + ", id=" + id + ", positions=" + positions + '}';
     }
 
 }
